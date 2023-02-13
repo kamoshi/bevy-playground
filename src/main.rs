b610c8070a2f7e06aaea652f9963ce8bd217e981
Maciej Jur <maciej@kamoshi.org>
2023-02-13 23:16:33 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use bevy::prelude::*;
+
+
+const HEIGHT: f32 = 720.0;
+const WIDTH: f32 = 1280.0;
+
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(Color::rgb(0.2, 0.2, 0.2)))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                width: WIDTH,
+                height: HEIGHT,
+                title: "Tower Defense".to_string(),
+                resizable: false,
+                ..Default::default()
+            },
+            ..Default::default()
+        }))
+        .add_startup_system(spawn_camera)
+        .add_startup_system(spawn_basic_scene)
+        .run();
+}
+
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
+    });
+}
+
+fn spawn_basic_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>
+) {
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..Default::default()
+    });
+}
+
+
